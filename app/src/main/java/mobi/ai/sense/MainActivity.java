cebfb40be385c2034ce072bbf1bd59f2c51f6b77
webster <inbox.websense@gmail.com>
2019-08-27 14:26:32 -0300

    Adicionando Método para Clique de Botão enviar.

--- app/src/main/java/mobi/ai/sense/MainActivity.java
+++ app/src/main/java/mobi/ai/sense/MainActivity.java
@@ -1,5 +1,9 @@
 package mobi.ai.sense;
 
+import android.content.Intent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -10,4 +14,12 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void sendMessage(View view) {
+        //Intent intent = new Intent(this, DisplayMessageActivity.class);
+        //startActivity(intent);
+    }
+
+
+
 }
