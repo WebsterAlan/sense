fc08ea66229426d3efadce8aa78a5b585c571ce7
webster <inbox.websense@gmail.com>
2019-08-29 15:44:53 -0300

    Comentário de criação de sessão com classe SharedPreferences

--- app/src/main/java/mobi/ai/sense/MainActivity.java
+++ app/src/main/java/mobi/ai/sense/MainActivity.java
@@ -1,11 +1,13 @@
 package mobi.ai.sense;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,6 +20,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void sendMessage(View view) {
+
         Intent intent = new Intent(this, DisplayMessageActivity.class);
         EditText editText4 = (EditText) findViewById(R.id.editText4);
         EditText editText5 = (EditText) findViewById(R.id.editText5);
@@ -33,6 +36,10 @@ public class MainActivity extends AppCompatActivity {
         //startActivityForResult();
     }
 
+    public void iniciarSessao(){
+
+        //SharedPreferences sharedPreferences = getApplicationContext().getSharedPreferences();
+    }
 
 
 }
