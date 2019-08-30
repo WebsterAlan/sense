6ae3d9a068e0224a380e04326a43782cf3288a6e
webster <inbox.websense@gmail.com>
2019-08-30 18:29:58 -0300

    Criação de atividade painel para listagem de produtos por categoria

---
+++ app/src/main/java/mobi/ai/sense/view/PanelActivity.java
@@ -0,0 +1,14 @@
+package mobi.ai.sense.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import mobi.ai.sense.R;
+
+public class PanelActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_products);
+    }
+}
