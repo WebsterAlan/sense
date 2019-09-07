0e00515ef5f1ea7263078e17254597e5081566e0
webster <inbox.websense@gmail.com>
2019-09-07 02:04:18 -0300

    Instanciação de Sense Database e método de inserção de dados

--- app/src/main/java/mobi/ai/sense/conf/Dbhelper.java
+++ app/src/main/java/mobi/ai/sense/conf/Dbhelper.java
@@ -20,7 +20,7 @@ public class Dbhelper extends SQLiteOpenHelper {
     public static final int DATABASE_VERSION = 1;
     public static final String DATABASE_NAME = "SENSE_DATABASE";
 
-    public Dbhelper(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
+    public Dbhelper(Context context) {
         super(context, this.DATABASE_NAME, null, this.DATABASE_VERSION);
     }
 
@@ -34,12 +34,12 @@ public class Dbhelper extends SQLiteOpenHelper {
 
     @Override
     public void onCreate(SQLiteDatabase sqLiteDatabase) {
-        sqLiteDatabase.execSQL("");
+        sqLiteDatabase.execSQL(this.SQL_CREATE_POST);
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
-       sqLiteDatabase.execSQL("");
+       sqLiteDatabase.execSQL(this.SQL_DELETE_POSTS);
        this.onCreate(sqLiteDatabase);
     }
 }
--- app/src/main/java/mobi/ai/sense/model/LoginModel.java
+++ app/src/main/java/mobi/ai/sense/model/LoginModel.java
@@ -1,13 +1,19 @@
 package mobi.ai.sense.model;
 
+import mobi.ai.sense.conf.Dbhelper;
+import mobi.ai.sense.conf.SenseApplication;
+
+
 public class LoginModel {
 
-
+    /* Instanciação do banco de dados Sqlite */
+     Dbhelper dbhelper = new Dbhelper(SenseApplication.context);
 
 
     public boolean consultUser(String email, String password){
 
-
+      /* Método de inserção de dados */
+      this.dbhelper.getWritableDatabase();
 
 
 
