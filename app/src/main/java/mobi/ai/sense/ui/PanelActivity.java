9fd0446dbc8fe14b83f73f353cffab2fecbdef1d
webster <inbox.websense@gmail.com>
2019-09-08 20:18:48 -0300

    Vinculando atividade ao menu, por meio de MenuInflater

--- app/src/main/java/mobi/ai/sense/ui/PanelActivity.java
+++ app/src/main/java/mobi/ai/sense/ui/PanelActivity.java
@@ -1,5 +1,7 @@
 package mobi.ai.sense.ui;
 
+import android.view.Menu;
+import android.view.MenuInflater;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import mobi.ai.sense.R;
@@ -10,5 +12,13 @@ public class PanelActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_products);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.sense_menu, menu);
+        return true;
     }
 }
