f733d6bb97771bce3458c88f5df20fdc7338a86d
webster <inbox.websense@gmail.com>
2019-12-10 17:40:57 -0300

    Background e gerenciamento apartir da activity principal

---
+++ app/src/main/java/mobi/ai/sense/ui/MainActivity.java
@@ -0,0 +1,21 @@
+package mobi.ai.sense.ui;
+
+import android.content.Intent;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import mobi.ai.sense.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Intent intent = new Intent();
+    }
+}
