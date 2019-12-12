b8d9c90c9aeee064304dcb26e8846af9be2bc374
webster <inbox.websense@gmail.com>
2019-12-12 14:46:44 -0300

    Implementação de CallOnClick em MaterialCardView

--- app/src/main/java/mobi/ai/sense/ui/MenuActivity.java
+++ app/src/main/java/mobi/ai/sense/ui/MenuActivity.java
@@ -2,14 +2,26 @@ package mobi.ai.sense.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import com.google.android.material.card.MaterialCardView;
 import mobi.ai.sense.R;
 
 /* Menu responsável por ações no aplicativo, durante transições de atividades */
 public class MenuActivity extends AppCompatActivity {
 
+
+    MaterialCardView cardView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        cardView = (MaterialCardView) findViewById(R.id.card01);
+        boolean result = cardView.callOnClick();
     }
 }
