a32d48b42410b47c22a3723faa6b0738e9769584
webster <inbox.websense@gmail.com>
2019-12-16 03:13:36 -0300

    Adição de BulletinActivity para relatórios e ocorrências

---
+++ app/src/main/java/mobi/ai/sense/ui/BulletinActivity.java
@@ -0,0 +1,14 @@
+package mobi.ai.sense.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import mobi.ai.sense.R;
+
+public class BulletinActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bulletin);
+    }
+}
